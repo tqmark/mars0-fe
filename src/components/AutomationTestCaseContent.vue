f719b4e65a43a859c0d27204ccac95d895d55e8f
Mark Tran <marktran@192.168.2.9>
2025-06-27 09:58:10 +0700

    Updated analyze spec view

--- src/components/AutomationTestCaseContent.vue
+++ src/components/AutomationTestCaseContent.vue
@@ -5,7 +5,7 @@
       v-if="loading"
       class="fixed inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50"
     >
-      <img src="@/assets/cua4_loading.gif" alt="Loading" class="max-w-1/4" />
+      <img src="@/assets/cua_new3.gif" alt="Loading" class="max-w-3/12" />
     </div>
 
     <div class="flex justify-between items-center mb-4">
@@ -196,10 +196,10 @@
 import type { PropType } from 'vue'
 import axios from 'axios'
 import 'prismjs/themes/prism.css'
-import Prism from 'prismjs';
-import 'prismjs/themes/prism.css'; // Default theme, you can choose another
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'; // Line numbers CSS
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css' // Default theme, you can choose another
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css' // Line numbers CSS
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
 import CodeBlock from '@/components/CodeBlock.vue' // Line numbers plugin
 export default {
   components: { CodeBlock },
