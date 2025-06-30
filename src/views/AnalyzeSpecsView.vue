6a9151e2bc0cae6de0966edb5f10c9b2e7bcfc4d
dat.nv <dat.nv@linecorp.com>
2025-06-30 11:18:49 +0900

    feat: update multiple category reviews for each screen

--- src/views/AnalyzeSpecsView.vue
+++ src/views/AnalyzeSpecsView.vue
@@ -57,38 +57,43 @@
           </td>
           <td class="p-3 flex flex-col">
             <div
-              class="flex w-full items-center justify-between mb-4 font-bold pb-4 pt-4 pl-4 bg-gray-100 rounded-md"
+              v-for="(content, cIndex) in review.contents"
+              :key="cIndex"
             >
-              <h2 v-if="review.category !== 'ui_ux'">
-                {{
-                  review.category
-                    .split('_')
-                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                    .join(' ')
-                }}
-              </h2>
-              <h2 v-else>UI/UX</h2>
+              <div
+                class="flex w-full items-center justify-between mb-4 font-bold pb-4 pt-4 pl-4 bg-gray-100 rounded-md"
+              >
+                <h2 v-if="content.category !== 'ui_ux'">
+                  {{
+                    content.category
+                      .split('_')
+                      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                      .join(' ')
+                  }}
+                </h2>
+                <h2 v-else>UI/UX</h2>
 
-              <img
-                :src="arrowDownRedIcon"
-                @click="toggleCollapse(index)"
-                :alt="review.collapsed ? 'Expand' : 'Collapse'"
-                :class="review.collapsed ? 'rotate-180 pl-4' : 'pr-4 stroke-red'"
-              />
-            </div>
-            <div
-              v-for="(recommendation, rIndex) in review.recommendations"
-              :key="rIndex"
-              class="mb-4"
-            >
-              <h4 class="font-semibold text-header pb-4">{{ recommendation.title }}</h4>
-              <ul class="list-disc list-inside pl-6 pb-6 font-light sub-text">
-                <template v-for="(detail, dIndex) in recommendation.details" :key="dIndex">
-                  <li v-if="!review.collapsed || dIndex < 2">
-                    {{ detail }}
-                  </li>
-                </template>
-              </ul>
+                <img
+                  :src="arrowDownRedIcon"
+                  @click="toggleCollapse(index)"
+                  :alt="review.collapsed ? 'Expand' : 'Collapse'"
+                  :class="review.collapsed ? 'rotate-180 pl-4' : 'pr-4 stroke-red'"
+                />
+              </div>
+              <div
+                v-for="(recommendation, rIndex) in content.recommendations"
+                :key="rIndex"
+                class="mb-4"
+              >
+                <h4 class="font-semibold text-header pb-4">{{ recommendation.title }}</h4>
+                <ul class="list-disc list-inside pl-6 pb-6 font-light sub-text">
+                  <template v-for="(detail, dIndex) in recommendation.details" :key="dIndex">
+                    <li v-if="!review.collapsed || dIndex < 2">
+                      {{ detail }}
+                    </li>
+                  </template>
+                </ul>
+              </div>
             </div>
           </td>
         </tr>
@@ -108,10 +113,12 @@ import arrowDownRedIcon from '@/assets/icondown-red.svg'
 interface Review {
   screen: string
   screenImage: string
-  category: string
-  recommendations: Array<{
-    title: string
-    details: string[]
+  contents: Array<{
+    category: string
+    recommendations: Array<{
+      title: string
+      details: string[]
+    }>
   }>
   collapsed: boolean // Add collapsed state
 }
@@ -143,24 +150,41 @@ const analyzeSpec = ref<AnalyzeSpec>({
     {
       screen: 'Login',
       screenImage: 'image1.png',
-      category: 'data',
-      recommendations: [
+      contents: [
         {
-          title: 'Unusual Situations to Consider',
-          details: [
-            'Users might try to interact with the pop-ups in unexpected ways, which could lead to errors.',
-            'Rare scenarios like system errors or network issues should also be considered during planning.',
-            'Users may not realize the limitations of the standardized pop-up format, so clear communication is key.',
+          category: "implementation",
+          recommendations: [
+            {
+              title: 'Unusual Situations to Consider',
+              details: [
+                'Users might try to interact with the pop-ups in unexpected ways, which could lead to errors.',
+                'Rare scenarios like system errors or network issues should also be considered during planning.',
+                'Users may not realize the limitations of the standardized pop-up format, so clear communication is key.',
+              ],
+            },
+            {
+              title: 'Unusual Situations to Consider 2',
+              details: [
+                'Users might try to interact with the pop-ups in unexpected ways, which could lead to errors.',
+                'Rare scenarios like system errors or network issues should also be considered during planning.',
+                'Users may not realize the limitations of the standardized pop-up format, so clear communication is key.',
+              ],
+            }
           ],
         },
         {
-          title: 'Unusual Situations to Consider 2',
-          details: [
-            'Users might try to interact with the pop-ups in unexpected ways, which could lead to errors.',
-            'Rare scenarios like system errors or network issues should also be considered during planning.',
-            'Users may not realize the limitations of the standardized pop-up format, so clear communication is key.',
-          ],
-        },
+          category: "test_validation",
+          recommendations: [
+            {
+              title: "How can we verify that the login validation messages are displayed correctly?",
+              details: [
+                "Relevant text: If left empty or mistyped, inline error messages should appear.",
+                "Why this would be asked: The specification does not define the exact conditions under which each error message should appear.",
+                "Recommendation: Provide a detailed list of scenarios and expected error messages for each case."
+              ]
+            }
+          ]
+        }
       ],
       collapsed: false, // Initialize the collapsed state
     },
