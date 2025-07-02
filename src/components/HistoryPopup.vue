6f1548d945a0ac19504e394de89db4dbd01e0de4
Mark Tran <marktran@VN-LW14085-M.local>
2025-07-02 15:22:49 +0900

    Bump up version

--- src/components/HistoryPopup.vue
+++ src/components/HistoryPopup.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-[64px] right-[64px] w-64 h-full bg-white shadow-lg border border-gray-200 rounded-md"
+    class="fixed top-[64px] right-[64px] w-80 h-full bg-white shadow-lg border border-gray-200 rounded-md"
   >
     <div class="flex justify-between items-center p-4 border-b">
       <h1 class="font-bold text-2xl">History</h1>
@@ -9,14 +9,14 @@
       </button>
     </div>
     <div class="p-4">
-      <h3 class="font-bold mb-2">Today</h3>
+      <h2 class=" text-xl font-bold mb-2">Today</h2>
       <ul>
-        <li class="bg-red-100 p-2 mb-2 rounded">Generate test case 3</li>
+        <li class="bg-[#fff2f2] p-4 my-6 rounded">Generate test case 3</li>
       </ul>
-      <h3 class="font-bold mb-2">Yesterday</h3>
+      <h2 class=" text-xl font-bold mb-2">Yesterday</h2>
       <ul>
-        <li class="bg-gray-100 p-2 mb-2 rounded">Analyze Topic 2</li>
-        <li class="bg-gray-100 p-2 mb-2 rounded">Analyze Topic 1</li>
+        <li class="bg-fafafb p-4 my-6 rounded">Analyze Topic 2</li>
+        <li class="bg-fafafb p-4 my-6 rounded">Analyze Topic 1</li>
       </ul>
     </div>
   </div>
@@ -40,4 +40,7 @@ export default {
 
 <style scoped>
 /* Add any additional styles here */
+.bg-fafafb {
+  background-color: #fafafb;
+}
 </style>
